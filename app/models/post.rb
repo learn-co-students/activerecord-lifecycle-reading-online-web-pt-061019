1b54976ec1da02951a7ad2390bd2f7526e7092db
Chris Junker <elevatemedfit@gmail.com>
2019-11-04 10:12:59 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,19 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case #whenever rails persists Post models to the db(#save,#create) this code will get run.
+
+  before_save :email_author_about_post #something that does not change a model.
+
   private
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
     end
+  end
+
+  def email_author_about_post
   end
 
   def make_title_case
